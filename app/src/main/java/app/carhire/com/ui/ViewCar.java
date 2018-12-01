7226ef2f1a9c7e701879cfd625d83c250f3df261
Brackie <muimikieleko@gmail.com>
2018-12-02 02:43:02 +0300

    Posts data to database and pulls for viewing

---
+++ app/src/main/java/app/carhire/com/ui/ViewCar.java
@@ -0,0 +1,141 @@
+package app.carhire.com.ui;
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import app.carhire.com.R;
+
+public class ViewCar extends AppCompatActivity {
+
+    private ImageView displayImage, bookCar;
+    private ProgressBar loadCarDetails;
+    private ListView carDetailsList;
+    private DatabaseReference rootDbRef;
+    private String car_id, owner_id, image_url;
+    private ArrayList<String> carDetails;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_car);
+
+        //get extras from intent
+        car_id = getIntent().getExtras().getString("car_id");
+        owner_id = getIntent().getExtras().getString("owner_id");
+        image_url = getIntent().getExtras().getString("image_url");
+
+        //set up toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //initialize variables
+        displayImage = (ImageView)findViewById(R.id.display_image);
+        loadCarDetails = (ProgressBar) findViewById(R.id.load_car_details);
+        carDetailsList = (ListView)findViewById(R.id.car_details_list);
+        bookCar = (ImageView)findViewById(R.id.book_car);
+        rootDbRef = FirebaseDatabase.getInstance().getReference();
+        carDetails = new ArrayList<String>();
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,carDetails);
+
+        //handle item clicks
+        bookCar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //method calls
+        getCarDetails();
+
+        //update ui
+        if (!image_url.equals("No Image"))
+        {
+            Glide.with(this).load(image_url).listener(new RequestListener<Drawable>() {
+                @Override
+                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                    displayImage.setImageResource(R.drawable.car_placeholder);
+                    return false;
+                }
+
+                @Override
+                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+
+                    return false;
+                }
+            }).into(displayImage);
+        }
+        carDetailsList.setAdapter(adapter);
+
+    }
+
+    private void getCarDetails() {
+
+        rootDbRef.child("cars").child(car_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                carDetails.add(dataSnapshot.child("car_make").getValue(String.class) + " " + dataSnapshot.child("car_model").getValue(String.class));
+                carDetails.add("Owner : " + dataSnapshot.child("car_owner").getValue(String.class));
+                carDetails.add("Engine : " + dataSnapshot.child("engine_size").getValue(String.class));
+                carDetails.add("Fuel : " + dataSnapshot.child("fuel_type").getValue(String.class));
+                carDetails.add("Transmission : " + dataSnapshot.child("car_transmission").getValue(String.class));
+                carDetails.add("Capacity : " + dataSnapshot.child("car_capacity").getValue(String.class));
+                carDetails.add("Rating : " + dataSnapshot.child("car_rating").getValue(String.class));
+                carDetails.add("Accessories : " + dataSnapshot.child("car_accessories").getValue(String.class));
+                carDetails.add("Hiring rate : " + dataSnapshot.child("hire_rate").getValue(String.class));
+
+                adapter.notifyDataSetChanged();
+
+                loadCarDetails.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loadCarDetails.setVisibility(View.GONE);
+                Toast.makeText(ViewCar.this, "Couldn't fetch car details", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id  = item.getItemId();
+
+        if (id == android.R.id.home)
+        {
+            finish();
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/app/carhire/com/ui/ViewCars.java
@@ -0,0 +1,151 @@
+package app.carhire.com.ui;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import app.carhire.com.R;
+import app.carhire.com.adapters.ViewCarsAdapter;
+import app.carhire.com.models.CarModel;
+
+public class ViewCars extends AppCompatActivity {
+
+    private TextView welcome, noAvailableCars;
+    private ListView carList;
+    private ProgressBar loadCars;
+    private ViewCarsAdapter carsAdapter;
+    private ArrayList<CarModel> availableCars;
+    private DatabaseReference rootDbRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cars);
+
+        //set up toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //initialize variables
+        welcome = (TextView)findViewById(R.id.welcome);
+        noAvailableCars = (TextView)findViewById(R.id.no_available_cars);
+        carList = (ListView)findViewById(R.id.car_list);
+        loadCars = (ProgressBar)findViewById(R.id.load_available_vehicles);
+        availableCars = new ArrayList<>();
+        carsAdapter = new ViewCarsAdapter(this,availableCars);
+        rootDbRef = FirebaseDatabase.getInstance().getReference();
+
+        //handle item clicks
+        carList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String car_id = availableCars.get(i).getCarId();
+                String owner_id = availableCars.get(i).getOwnerId();
+                String image_url = availableCars.get(i).getImageUrl();
+
+                Intent viewCar = new Intent(ViewCars.this, ViewCar.class);
+                viewCar.putExtra("car_id",car_id);
+                viewCar.putExtra("owner_id",owner_id);
+                viewCar.putExtra("image_url",image_url);
+                startActivity(viewCar);
+
+            }
+        });
+
+        //update ui
+        carList.setAdapter(carsAdapter);
+
+        //method call
+        getAvailableCars();
+
+    }
+
+    //method to get available cars on firebase database
+    private void getAvailableCars(){
+
+        rootDbRef.child("cars").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                availableCars.clear();
+
+                for (DataSnapshot ds : dataSnapshot.getChildren())
+                {
+                    CarModel carModel = new CarModel();
+                    carModel.setCarId(ds.child("car_id").getValue(String.class));
+                    carModel.setImageUrl(ds.child("image_url").getValue(String.class));
+                    carModel.setCarMake(ds.child("car_make").getValue(String.class));
+                    carModel.setCarModel(ds.child("car_model").getValue(String.class));
+                    carModel.setCarOwner(ds.child("car_owner").getValue(String.class));
+                    carModel.setEngineSize(ds.child("engine_size").getValue(String.class));
+                    carModel.setCarTransmission(ds.child("car_transmission").getValue(String.class));
+                    carModel.setCarRating(ds.child("car_rating").getValue(String.class));
+                    carModel.setHireRate(ds.child("hire_rate").getValue(String.class));
+
+                    availableCars.add(carModel);
+                }
+
+                carsAdapter.notifyDataSetChanged();
+                loadCars.setVisibility(View.GONE);
+
+                if (availableCars.isEmpty())
+                {
+                    noAvailableCars.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loadCars.setVisibility(View.GONE);
+                Toast.makeText(ViewCars.this, "Couldn't fetch available cars", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.view_cars_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.post_car)
+        {
+            startActivity(new Intent(this,PostCar.class));
+        }
+        else if (id == R.id.review_account)
+        {
+
+        }
+        else if (id == R.id.exit)
+        {
+            finish();
+        }
+        return true;
+    }
+}

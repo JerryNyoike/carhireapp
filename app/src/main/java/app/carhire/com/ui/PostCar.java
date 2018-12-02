332f4fcc444413e1d51a953a8fa28591762bec7d
Jerry <jngugi31@gmail.com>
2018-12-02 10:00:45 +0300

    Added Firebase Authentication

--- app/src/main/java/app/carhire/com/ui/PostCar.java
+++ app/src/main/java/app/carhire/com/ui/PostCar.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import app.carhire.com.R;
+
 public class PostCar extends AppCompatActivity {
 
     @Override
